26a8a07a2040189b040d6af2bab4a3d26a3b78bb
QuincyChen <mail@quincychen.cn>
2021-11-08 14:22:34 +0800

    style: utils/is formatting

--- src/utils/is.ts
+++ src/utils/is.ts
@@ -1,17 +1,15 @@
 export const isArray = Array.isArray;
 
+/** Whether the variable effective. */
+export const isEffective = (variable: any): boolean =>
+  typeof variable !== 'undefined' && variable !== null;
+
 export const isBrowser = typeof window !== 'undefined' && typeof document !== 'undefined';
+
 /** Whether the environment support -webkit-line-clamp. */
 export const isSupportNativeEllipsis =
   isBrowser && typeof document.body.style.webkitLineClamp !== 'undefined';
 
-export const isExistentAndEqualTo = (value: any, target: any) => {
-  return value && value === target;
-};
 /** Whether the environment support window.requestAnimationFrame. */
 export const isSupportRequestAnimationFrame =
   isBrowser && typeof window.requestAnimationFrame !== 'undefined';
-
-/** Whether the variable effective. */
-export const isEffective = (variable: any): boolean =>
-  typeof variable !== 'undefined' && variable !== null;
